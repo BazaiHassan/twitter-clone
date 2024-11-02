a551b8b88d9d01d9c4ac612dce0bdc86fda8028f
bazai hassan <bazaee.hassan@gmail.com>
2024-11-02 22:12:37 +0330

    backend: register and prisma config

---
+++ server/api/auth/register.post.js
@@ -0,0 +1,42 @@
+import { sendError } from "h3";
+import { createUser } from "~/server/db/users";
+import { userTransformer } from "~/server/transformers/user";
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const { username, password, repeatPassword, email, name } = body;
+
+  // Validation
+  if (!username || !password || !repeatPassword || !email || !name) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: "Invalid Params",
+      })
+    );
+  }
+
+  if (password != repeatPassword) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: "Passwords do not match!",
+      })
+    );
+  }
+
+  const userData = {
+    username,
+    password,
+    email,
+    name,
+  };
+
+  const user = await createUser(userData);
+
+  return {
+    body: userTransformer(user),
+  };
+});
