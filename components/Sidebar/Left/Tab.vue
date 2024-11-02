b8b395d4ef4c8346195cb1acb2cb7bd181e9bd64
bazai hassan <bazaee.hassan@gmail.com>
2024-11-02 16:29:24 +0330

    dev:left sidebar

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink
+    to="#"
+    class="flex items-center text-black rounded-full p-3 w-min hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white"
+    :class="defaultTransition"
+  >
+    <div class="w-6 h-6 text-dark">
+      <slot name="icon"></slot>
+    </div>
+    <div class="ml-2 hidden xl:block" :class="textClasses">
+      <slot name="name"></slot>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const textClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal"
+);
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <!-- LOGO START -->
+    <div
+      class="p-2 my-2 w-min hover:bg-blue-50 rounded-full dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+    <!-- LOGO END -->
+
+    <div class="space-y-3">
+      <!-- Sidebar Item 1-->
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name> Home </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 2-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <HashtagIcon />
+        </template>
+        <template v-slot:name> Explorer </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 3-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <BellIcon />
+        </template>
+        <template v-slot:name> Notifications </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 4-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <InboxIcon />
+        </template>
+        <template v-slot:name> Messages </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 5-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <BookmarkIcon />
+        </template>
+        <template v-slot:name> Bookmark </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 6-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <DocumentTextIcon />
+        </template>
+        <template v-slot:name> Lists </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 7-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name> Profile </template>
+      </SidebarLeftTab>
+
+      <!-- Sidebar Item 8-->
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <EllipsisHorizontalCircleIcon />
+        </template>
+        <template v-slot:name> More </template>
+      </SidebarLeftTab>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/24/solid";
+
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+  EllipsisHorizontalCircleIcon,
+} from "@heroicons/vue/24/outline";
+const { defaultTransition } = useTailwindConfig();
+</script>
