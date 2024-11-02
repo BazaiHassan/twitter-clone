8d1064fef1b6d39277dc733f4fc4fcfe56a0358e
bazai hassan <bazaee.hassan@gmail.com>
2024-11-02 20:57:34 +0330

    dev:main section is on going

--- app.vue
+++ app.vue
@@ -14,7 +14,7 @@
 
           <!-- Main Content -->
           <main class="col-span-12 md:col-span-8 xl:col-span-6" >
-            
+            <RouterView></RouterView>
           </main>
 
           <!-- Right Sidebar -->
---
+++ components/MainSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="border-x" :class="twitterBorderColor">
+    <div
+      class="sticky top-0 px-4 py-3 bg-white/80 backdrop-blur-md dark:bg-dim-900/80"
+    >
+      <h2 class="text-xl font-bold text-gray-800 dark:text-gray-100">
+        {{ props.title }}
+      </h2>
+      <div
+        :class="twitterBorderColor"
+        class="flex items-center justify-between p-4 border-b flex-col"
+        v-if="props.loading"
+      >
+        <UISpinner />
+      </div>
+      <div v-else>
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const { twitterBorderColor } = useTailwindConfig();
+</script>
